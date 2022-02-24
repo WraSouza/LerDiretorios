31e6dc0d601a3c5dd7ca6162944ad7cf70706d1b
Wladimir Ribeiro de Souza <wladimirsouza@outlook.com>
2022-02-24 13:10:31 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var path = @"C:\Users\wladimir.souza\Documents\DIO\Directory_DirectoryInfo\globo1";
+
+LerDiretorios(path);
+Console.WriteLine("Digite [ENTER] para finalizar");
+
+static void LerDiretorios(string path)
+{
+    if(Directory.Exists(path))
+    {
+        var diretorios = Directory.GetDirectories(path,"*", SearchOption.AllDirectories);
+    foreach(var dir in diretorios)
+    {
+        var dirInfo = new DirectoryInfo(dir);
+        Console.WriteLine($"[Nome]:{dirInfo.Name}");
+         Console.WriteLine($"[Raiz]:{dirInfo.Root}");
+
+         if(dirInfo.Parent!=null)
+        Console.WriteLine($"[Pai]:{dirInfo.Parent.Name}");
+
+        Console.WriteLine("--------------");
+        
+    }
+    }else{
+        Console.WriteLine($"{path} Não Existe");
+    }
+    
+}
